30485c44aa8c399c7e01a43763245bba04207f42
sumitsawant1029 <sumitsawant1029@gmail.com>
2025-11-26 14:04:09 +0530

    Making Skills

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,12 +1,38 @@
 import Navbar from "./Navbar";
-
-function Skills(){
-    return(
+import './Skills.css';
+function Skills() {
+    return (
         <>
-            <Navbar/>
-            Skills
+            <Navbar />
+            <div style={{padding:'10px 10px'}}>
+                <div className="innerDivSkills">
+                    <div>
+                        1
+                    </div>
+                    <div>
+                        2
+                    </div>
+                    <div>
+                        3
+                    </div>
+                </div>
+                <div className="innerDivSkills">
+
+                    <div>
+                        4
+                    </div>
+                    <div>
+                        5
+                    </div>
+                    <div>
+                        6
+                    </div>
+                </div>
+
+
+            </div>
         </>
-        )
+    )
 }
 
 export default Skills;
