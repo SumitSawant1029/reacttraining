160e7b5ab16bb292f492459d567777f8676f1a78
sumitsawant1029 <sumitsawant1029@gmail.com>
2025-11-26 12:30:39 +0530

    ff

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,6 @@
 import Navbar from "./Navbar";
 import './Home.css'
-import photo from '../assets/Photo.png';
+import photo1 from '../assets/Photo1.png';
 
 function Home() {
     return (
@@ -11,15 +11,15 @@ function Home() {
 
                 <div>
                     <div>
-                        <a style={{ fontSize: 'x-large', fontFamily: "'Motorola ScreenType', sans-serif" }}>Hello,</a><br></br>
-                        <a style={{ fontSize: 'xxx-large', fontWeight: '800', fontFamily: "'Motorola ScreenType', sans-serif", color: '#423b3bff' }}>I'm Sumit Sawant</a><br />
-                        <a style={{ fontSize: 'xxx-large', fontFamily: "'Motorola ScreenType', sans-serif", color: '#F58486' }}>Software Developer</a><br />
-                        <a style={{ fontSize: 'x-large', fontFamily: "'Motorola ScreenType', sans-serif" }}>From India</a><br />
-                        <button style={{ width: '150px', height: '30px', backgroundColor: '#FF8882', border: '0px', color: 'whitesmoke', borderRadius: '5px' }} >Hire Me</button><br />
+                        <a style={{ fontSize: '4.2vh', fontFamily: "'Motorola ScreenType', sans-serif" , color: '#423b3bff' }}>Hello,</a><br></br>
+                        <a style={{ fontSize: '8.2vh', fontWeight: '800', fontFamily: "'Motorola ScreenType', sans-serif", color: '#423b3bff' }}>I'm Sumit Sawant</a><br />
+                        <a style={{ fontSize: '8.2vh', fontFamily: "'Motorola ScreenType', sans-serif", color: '#F58486' }}>Software Developer</a><br />
+                        <a style={{ fontSize: '4.2vh', fontFamily: "'Motorola ScreenType', sans-serif", color: '#423b3bff' }}>From India</a><br />
+                        <button style={{ width: '150px', height: '30px', backgroundColor: '#FF8882', border: '0px', color: 'whitesmoke', borderRadius: '5px',fontFamily: "'Motorola ScreenType', sans-serif",fontSize:"1.2rem" }} >Hire Me</button><br />
                     </div>
                 </div>
                 <div>
-                    <img src={photo} style={{ width: '80%', zIndex: '1' }} />
+                    <img src={photo1} style={{ width: '80%', zIndex: '1' }} />
                     <div className="portfolio"><a>Portfolio</a></div>
                 </div>
 
