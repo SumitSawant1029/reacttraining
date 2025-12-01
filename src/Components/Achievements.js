16120bccefafed993b9ff768a1f36dbb40cc1ef8
sumitsawant1029 <sumitsawant1029@gmail.com>
2025-12-01 16:18:00 +0530

    Version 1

--- src/Components/Achievements.js
+++ src/Components/Achievements.js
@@ -1,5 +1,5 @@
 import './Achievements.css';
-
+import image from '../assets/Learning-cuate.png';
 
 function Achievements() {
 
@@ -8,47 +8,47 @@ function Achievements() {
             <div className='achievementOuterDiv'>
 
                 <div id="achievementsImage">
-                    <img></img>
+                    <img src={image} style={{width:'30vw',padding:'45px 15px'}}></img>
                 </div>
                 <div id="achievements">
-                    <h1>Achievements</h1>
-                    <p>Lorem ncuj nu uu  uuu juj u ujj ujuujj ju u ju ju u ujuu  jjju ju ju uuj uj jju uj </p>
+                    <h1 className='header'>Achievements</h1>
+                    <p>A passionate Information Technology student with a strong foundation in software development, problem-solving, and teamwork. Experienced in hackathons and collaborative projects, with a proven ability to deliver innovative solutions under pressure.</p>
                     <div className="subdiv">
 
                         <div>
-                            <h2>Education</h2>
+                            <h2 className='header'>Education</h2>
                             <div>
-                                <a>2021-2025</a><br/>
-                                <a>Bachelor of Engineering (Information Technology)</a><br/>
-                                <a>Don Bosco Institute Of Technology</a><br/><br/>
+                                <a className='year'>2021-2025</a><br/>
+                                <a className='midheader'>Bachelor of Engineering (Information Technology)</a><br/>
+                                <a className='description'>Don Bosco Institute Of Technology</a><br/><br/>
                             </div>
                             <div>
-                                <a>2021</a><br/>
-                                <a>Higher Secondary Certificate</a><br/>
-                                <a>Sathaye College of Science</a><br/><br/>
+                                <a className='year'>2021</a><br/>
+                                <a className='midheader'>Higher Secondary Certificate</a><br/>
+                                <a className='description'>Sathaye College of Science</a><br/><br/>
                             </div>
                             <div>
-                                <a>2019</a><br/>
-                                <a>Secondary School Certificate</a><br/>
-                                <a>St Dominic Savio High School</a><br/><br/>
+                                <a className='year'>2019</a><br/>
+                                <a className='midheader'>Secondary School Certificate</a><br/>
+                                <a className='description'>St Dominic Savio High School</a><br/><br/>
                             </div>
                         </div>
                         <div>
-                            <h2>Awards</h2>
+                            <h2 className='header'>Awards</h2>
                             <div>
-                                <a>2021-2025</a>
-                                <a>Bachelor of Engineering (Information Technology)</a>
-                                <a>Don Bosco Institute Of Technology</a>
+                                <a className='year'>2023</a><br/>
+                                <a className='midheader'>Winner Of Smart India Hackathon</a><br/>
+                                <a className='description'>Worked On Centralized Database for Nasha Mukti Centre Project</a><br/><br/>
                             </div>
                             <div>
-                                <a>2021</a>
-                                <a>Higher Secondary Certificate</a>
-                                <a>Sathaye College of Science</a>
+                                <a className='year'>2021</a><br/>
+                                <a className='midheader'>Participated In Mumbai Hackathon</a><br/>
+                                <a className='description'>Sathaye College of Science</a><br/><br/>
                             </div>
                             <div>
-                                <a>2019</a>
-                                <a>Secondary School Certificate</a>
-                                <a>St Dominic Savio High School</a>
+                                <a className='year'>2019</a><br/>
+                                <a className='midheader'>Secondary School Certificate</a><br/>
+                                <a className='description'>St Dominic Savio High School</a><br/><br/>
                             </div>
                         </div>
                     </div>
